8f2cbc5c4c6bce6df2dfa49c59807e1b13b96298
vburmus <burmusvolod@gmail.com>
2023-09-27 11:56:08 +0200

    feat(aws-utils) added aws-utils application

---
+++ aws-utils/src/main/java/com/epam/esm/AwsUtilsApplication.java
@@ -0,0 +1,11 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsUtilsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsUtilsApplication.class, args);
+    }
+}
