be29f2fd5d90171e8f3419b1c7b01a2786cc7bf4
vburmus <burmusvolod@gmail.com>
2023-09-29 16:32:39 +0200

    feat(commerce-microservice-certificate) added certificate validation

--- commerce/src/main/java/com/epam/esm/utils/Validation.java
+++ commerce/src/main/java/com/epam/esm/utils/Validation.java
@@ -1,12 +1,25 @@
 package com.epam.esm.utils;
 
 
+import com.epam.esm.certificate.models.Certificate;
 import com.epam.esm.tag.models.Tag;
 import lombok.experimental.UtilityClass;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @UtilityClass
 public class Validation {
     public static boolean isTagValid(Tag tag) {
         return tag != null && tag.getName() != null && !tag.getName().isEmpty();
     }
+
+    public static boolean isValidCertificate(Certificate giftCertificate) {
+        return giftCertificate.getName() != null && !giftCertificate.getName().isEmpty() &&
+                giftCertificate.getDurationDate() != null &&
+                (LocalDateTime.now()).isBefore(giftCertificate.getDurationDate()) &&
+                giftCertificate.getPrice() != null && giftCertificate.getPrice().compareTo(BigDecimal.ZERO) > 0 &&
+                giftCertificate.getTags() != null;
+    }
+
 }
