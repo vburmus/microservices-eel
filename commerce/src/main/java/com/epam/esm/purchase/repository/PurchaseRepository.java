446da3c0a3546eb4a91a717d3155076470fe8573
vburmus <burmusvolod@gmail.com>
2023-09-30 15:04:06 +0200

    feat(commerce-microservice-purchase) created purchase repository and created relation with certificate

---
+++ commerce/src/main/java/com/epam/esm/purchase/repository/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package com.epam.esm.purchase.repository;
+
+import com.epam.esm.purchase.models.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+}
---
+++ commerce/src/main/java/com/epam/esm/purchasecertificate/model/PurchaseCertificate.java
@@ -0,0 +1,48 @@
+package com.epam.esm.purchasecertificate.model;
+
+import com.epam.esm.certificate.models.Certificate;
+import com.epam.esm.purchase.models.Purchase;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "purchase_certificates")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PurchaseCertificate {
+    @EmbeddedId
+    @JsonIgnore
+    private PurchaseCertificatePK purchaseCertificatePK;
+
+    @ManyToOne
+    @MapsId("certificateId")
+    private Certificate certificate;
+
+    @ManyToOne
+    @MapsId("purchaseId")
+    @JsonIgnore
+    private Purchase purchase;
+
+    private Integer quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        PurchaseCertificate that = (PurchaseCertificate) o;
+        return purchaseCertificatePK != null && Objects.equals(purchaseCertificatePK, that.purchaseCertificatePK);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(purchaseCertificatePK).append(certificate).append(purchase).append(quantity).toHashCode();
+    }
+}
