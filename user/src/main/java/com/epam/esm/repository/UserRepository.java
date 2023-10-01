b475e55614df46c333dbde07412d1159e321c097
vburmus <burmusvolod@gmail.com>
2023-10-02 00:08:31 +0200

    feat(user-microservice) added repository

---
+++ user/src/main/java/com/epam/esm/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.epam.esm.repository;
+
+import com.epam.esm.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
