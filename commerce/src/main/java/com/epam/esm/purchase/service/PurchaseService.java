acc215caad89e07f96993893eb3e482bc5bd6585
vburmus <burmusvolod@gmail.com>
2023-10-02 19:39:21 +0200

    fix(commerce-microservice-tag) reordered methods in service, renamed create and improved logic

--- commerce/src/main/java/com/epam/esm/purchase/service/PurchaseService.java
+++ commerce/src/main/java/com/epam/esm/purchase/service/PurchaseService.java
@@ -5,13 +5,13 @@ import com.epam.esm.certificate.service.CertificateService;
 import com.epam.esm.purchase.models.Purchase;
 import com.epam.esm.purchase.models.PurchaseCreationRequest;
 import com.epam.esm.purchase.models.PurchaseDTO;
+import com.epam.esm.purchase.models.PurchasePosition;
 import com.epam.esm.purchase.repository.PurchaseRepository;
 import com.epam.esm.purchasecertificate.model.PurchaseCertificate;
 import com.epam.esm.purchasecertificate.model.PurchaseCertificatePK;
 import com.epam.esm.purchasecertificate.repository.PurchaseCertificateRepository;
 import com.epam.esm.utils.EntityToDtoMapper;
 import com.epam.esm.utils.exceptionhandler.exceptions.NoSuchObjectException;
-import com.epam.esm.utils.exceptionhandler.exceptions.PurchasePositionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,7 +26,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.epam.esm.utils.Constants.PURCHASE_DOES_NOT_EXISTS_ID;
-import static com.epam.esm.utils.Constants.WRONG_POSITION_PURCHASE;
 
 @Service
 @RequiredArgsConstructor
@@ -37,7 +36,7 @@ public class PurchaseService {
     private final EntityToDtoMapper entityToDtoMapper;
 
     @Transactional
-    public PurchaseDTO createPurchase(PurchaseCreationRequest purchaseCreationRequest) {
+    public PurchaseDTO create(PurchaseCreationRequest purchaseCreationRequest) {
         Map<Certificate, Integer> certificateQuantityMap = createCertificateQuantityMap(purchaseCreationRequest);
         LocalDateTime now = LocalDateTime.now();
         Purchase savedPurchase = purchaseRepository.save(
@@ -54,25 +53,33 @@ public class PurchaseService {
         return entityToDtoMapper.toPurchaseDTO(savedPurchase);
     }
 
+    public Page<PurchaseDTO> readAll(Pageable pageable) {
+        return purchaseRepository.findAll(pageable).map(entityToDtoMapper::toPurchaseDTO);
+    }
+
+    public PurchaseDTO getById(long id) {
+        return purchaseRepository.findById(id).map(entityToDtoMapper::toPurchaseDTO)
+                .orElseThrow(() -> new NoSuchObjectException(String.format(PURCHASE_DOES_NOT_EXISTS_ID, id)));
+    }
+
     private Set<PurchaseCertificate> createPurchaseCertificateSet(Map<Certificate, Integer> certificateQuantityMap,
                                                                   Purchase savedPurchase) {
         return certificateQuantityMap.entrySet().stream().map(entry -> {
             Certificate certificate = entry.getKey();
+            Integer quantity = entry.getValue();
             PurchaseCertificatePK purchaseCertificatePK = new PurchaseCertificatePK(savedPurchase.getId(),
                     certificate.getId());
-            return purchaseCertificateRepository.save(new PurchaseCertificate(purchaseCertificatePK, certificate,
-                    savedPurchase,
-                    entry.getValue()));
+            return purchaseCertificateRepository.save(
+                    new PurchaseCertificate(purchaseCertificatePK, certificate, savedPurchase, quantity));
         }).collect(Collectors.toSet());
     }
 
     private Map<Certificate, Integer> createCertificateQuantityMap(PurchaseCreationRequest purchaseCreationRequest) {
         Map<Certificate, Integer> certificateQuantityMap = new HashMap<>();
-        for (Map.Entry<Long, Integer> certificateRequest : purchaseCreationRequest.certificateQuantity().entrySet()) {
-            Long certificateId = certificateRequest.getKey();
-            Integer quantity = certificateRequest.getValue();
-            Certificate certificate = entityToDtoMapper.toCertificate(certificateService.getById(certificateId));
-            certificateQuantityMap.put(certificate, quantity);
+        for (PurchasePosition position : purchaseCreationRequest.positions()) {
+            Certificate certificate =
+                    entityToDtoMapper.toCertificate(certificateService.getById(position.certificateId()));
+            certificateQuantityMap.put(certificate, position.quantity());
         }
         return certificateQuantityMap;
     }
@@ -82,21 +89,10 @@ public class PurchaseService {
         for (Map.Entry<Certificate, Integer> entry : certificateQuantityMap.entrySet()) {
             Certificate certificate = entry.getKey();
             Integer quantity = entry.getValue();
-            if (certificate == null || quantity == null || quantity <= 0)
-                throw new PurchasePositionException(WRONG_POSITION_PURCHASE);
             BigDecimal certificateCost = certificate.getPrice();
             BigDecimal lineItemCost = certificateCost.multiply(BigDecimal.valueOf(quantity));
             totalCost = totalCost.add(lineItemCost);
         }
         return totalCost;
-    }
-
-    public Page<PurchaseDTO> readAll(Pageable pageable) {
-        return purchaseRepository.findAll(pageable).map(entityToDtoMapper::toPurchaseDTO);
-    }
-
-    public PurchaseDTO getById(long id) {
-        return purchaseRepository.findById(id).map(entityToDtoMapper::toPurchaseDTO)
-                .orElseThrow(() -> new NoSuchObjectException(String.format(PURCHASE_DOES_NOT_EXISTS_ID, id)));
     }
 }
