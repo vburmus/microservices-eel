6dccb2f8f5680853dd20ed1c9099c50da66d75b8
vburmus <burmusvolod@gmail.com>
2023-10-02 19:31:56 +0200

    feat(commerce-microservice) added jakarta validation

--- commerce/src/main/java/com/epam/esm/purchase/models/PurchaseCreationRequest.java
+++ commerce/src/main/java/com/epam/esm/purchase/models/PurchaseCreationRequest.java
@@ -1,6 +1,17 @@
 package com.epam.esm.purchase.models;
 
-import java.util.Map;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
-public record PurchaseCreationRequest(String description, Long userId, Map<Long, Integer> certificateQuantity) {
+import static com.epam.esm.utils.Constants.*;
+
+public record PurchaseCreationRequest(
+        @Size(max = 255, message = DESCRIPTION_SHOULD_BE_LESS_THAN_255_CHARS)
+        String description,
+        @NotNull(message = USER_ID_CANNOT_BE_EMPTY)
+        Long userId,
+        @NotEmpty(message = PURCHASE_SHOULD_CONTAIN_CERTIFICATES)
+        Set<PurchasePosition> positions) {
 }
