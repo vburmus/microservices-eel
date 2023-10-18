7fc0ce49e09151260107e6450158fcb350ff5552
vburmus <burmusvolod@gmail.com>
2023-10-19 00:43:24 +0200

    feat(auth_microservice) added feign client

---
+++ auth/src/main/java/com/epam/esm/utils/openfeign/UserFeignClient.java
@@ -0,0 +1,17 @@
+package com.epam.esm.utils.openfeign;
+
+import com.epam.esm.auth.models.CreateUserRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@FeignClient(name = "user-service", configuration = CustomFeignClientConfiguration.class)
+public interface UserFeignClient {
+    @PostMapping(value = "api/v1/users", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    User create(@RequestPart("user") CreateUserRequest createUserRequest,
+                @RequestPart("image") MultipartFile image);
+
+    @GetMapping("api/v1/users/by-email")
+    User getByEmail(@RequestParam("email") String email);
+}
