65ca39cf404544afd9defe3fd3dccc3e5a332ed8
vburmus <burmusvolod@gmail.com>
2023-11-08 15:34:29 +0100

    fix(dev_notification) added actuator dependency and removed template from config

--- commerce/src/main/java/com/epam/esm/utils/amqp/MQConfig.java
+++ commerce/src/main/java/com/epam/esm/utils/amqp/MQConfig.java
@@ -2,9 +2,10 @@ package com.epam.esm.utils.amqp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,12 +41,5 @@ public class MQConfig {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter(objectMapper);
-    }
-
-    @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        return template;
     }
 }
