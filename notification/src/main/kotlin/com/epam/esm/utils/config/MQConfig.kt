4c8ed5c9889b00590a185f597566d60f1b2bb528
vburmus <burmusvolod@gmail.com>
2023-11-08 15:51:20 +0100

    fix(dev_notification) configuration changes

--- notification/src/main/kotlin/com/epam/esm/utils/config/MQConfig.kt
+++ notification/src/main/kotlin/com/epam/esm/utils/config/MQConfig.kt
@@ -2,9 +2,6 @@ package com.epam.esm.utils.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import org.springframework.amqp.core.AmqpTemplate
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.context.annotation.Bean
@@ -14,11 +11,4 @@ import org.springframework.context.annotation.Configuration
 class MQConfig {
     @Bean
     fun messageConverter(): MessageConverter = Jackson2JsonMessageConverter(ObjectMapper().registerModules(JavaTimeModule()))
-
-    @Bean
-    fun template(connectionFactory: ConnectionFactory?): AmqpTemplate {
-        val template = RabbitTemplate(connectionFactory)
-        template.messageConverter = messageConverter()
-        return template
-    }
 }
