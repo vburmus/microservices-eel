a2d5a1d0cdbd3f77708abc3715f69fdafe9ea53e
vburmus <burmusvolod@gmail.com>
2023-11-14 18:33:24 +0100

    fix(dev_configuration)added notification service to configuration

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -13,4 +13,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail:3.1.3")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.1.3")
     implementation("org.thymeleaf.extras:thymeleaf-extras-java8time:3.0.4.RELEASE")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
