c49e90427806eae9db7fe67793f112c3f5394e8e
vburmus <burmusvolod@gmail.com>
2023-11-27 23:39:55 +0100

    fix(auth) changed mapper and class classes to records

--- auth/src/main/java/com/epam/esm/utils/EntityToDtoMapper.java
+++ auth/src/main/java/com/epam/esm/utils/EntityToDtoMapper.java
@@ -1,6 +1,6 @@
 package com.epam.esm.utils;
 
-import com.epam.esm.utils.openfeign.CreateUserRequest;
+import com.epam.esm.utils.amqp.CreateUserRequest;
 import com.epam.esm.auth.models.RegisterRequest;
 import com.epam.esm.credentials.model.Credentials;
 import com.epam.esm.model.AuthenticatedUser;
--- auth/src/main/java/com/epam/esm/utils/amqp/EmailValidationMessage.java
+++ auth/src/main/java/com/epam/esm/utils/amqp/EmailValidationMessage.java
@@ -1,13 +1,4 @@
 package com.epam.esm.utils.amqp;
 
-import lombok.*;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@ToString
-public class EmailValidationMessage {
-    private String email;
-    private String activationUrl;
+public record EmailValidationMessage(String email, String activationUrl) {
 }
